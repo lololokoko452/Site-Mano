de446b382c0caa7725b1cb2e21bb36ae870bdcab
lololokoko452 <mano.raichon@orange.fr>
2023-10-25 15:05:04 +0200

    [Site] Ajout du template education

--- src/Controller/site/HomeController.php
+++ src/Controller/site/HomeController.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Session\Session;
-
+use Symfony\Component\HttpKernel\KernelInterface;
 
 class HomeController extends AbstractController
 {
